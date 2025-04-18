be03df272f5c617b761c818e8cac0768c0713757
Bob Newhart <bob@bob.com>
2025-04-18 23:30:04 +0000

    Removed default var, added tf workspace tag

--- variables.tf
+++ variables.tf
@@ -10,35 +10,30 @@ variable "region" {
 
 variable "prefix" {
   type        = string
-  description = "(Optional) Prefix to use for all resources in this module. Default: globo-dev"
-  default     = "globo-dev"
+  description = "(Required) Prefix to use for all resources in this module. 
+
 }
 
 variable "cidr_block" {
   type        = string
-  description = "(Optional) The CIDR block for the VPC. Default:10.42.0.0/16"
-  default     = "10.42.0.0/16"
+  description = "(Required) The CIDR block for the VPC. 
+
 }
 
 variable "environment" {
   type        = string
-  description = "(Optional) Environment to use for all resources in this module. Default: dev"
-  default     = "development"
-
+  description = "(Required) Environment to use for all resources in this module."
+ 
 }
 
 variable "billing_code" {
   type        = string
   description = "(Required) Billing code to use for all resources in this module."
+
 }
 
 variable "public_subnets" {
   type        = map(string)
-  description = "(Optional) Map of public subnets to create with CIDR blocks. Key will be used as subnet name with prefix. Default: {subnet-1 ="
-  default = {
-    public-1 = "10.42.10.0/24"
-    public-2 = "10.42.11.0/24"
-    public-3 = "10.42.12.0/24"
-  }
+  description = "(Required) Map of public subnets to create with CIDR blocks. Key will be used as subnet name with prefix. 
 }
 
