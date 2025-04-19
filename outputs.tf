171d4411933afcf7586baa3f204fcbd6f491d7db
Bob Newhart <bob@bob.com>
2025-04-19 16:27:15 +0000

    Add tfe outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 ##################################################################################
 # OUTPUT
 ##################################################################################
+
+# This output contains the IDs of the VPC
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.main.vpc_id
+}
+
+# This output contains the IDs of the public subnets
+output public_subnets {
+  description = "List of public subnets"
+  value       = module.main.public_subnets
+}
